ce49acf7c6a4c433205fbb33fde2ee5178429e0d
mattiacallara <mattiacallara0@gmail.com>
2026-06-11 00:54:58 +0200

    feat: implement lamp toggle functionality

--- main.js
+++ main.js
@@ -1,2 +1,13 @@
 const whiteLamp = document.querySelector("#whiteLamp");
 const button = document.querySelector("#btn");
+
+button.addEventListener("click", () => {
+        
+    if(whiteLamp.src.includes('white_lamp')){
+            whiteLamp.src = "img/yellow_lamp.png";
+            button.textContent = "Spegni";
+    } else {
+            whiteLamp.src = "img/white_lamp.png";
+            button.textContent = "Accendi";
+    }
+})
